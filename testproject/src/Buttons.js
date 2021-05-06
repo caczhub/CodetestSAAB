969deb69ffaeee183704e56bb2f218977faef734
carzetdesktop <carl.a.zettergren@gmail.com>
2021-05-06 13:20:30 +0200

    Buttons now update state of parent in reacty-way

--- testproject/src/Buttons.js
+++ testproject/src/Buttons.js
@@ -1,14 +1,14 @@
 //jag hade nog egentligen delat upp detta i två olika knappar som jag kunde återanvända bättre, men det behövs inte för detta projekts scope.
-function Buttons(){
+function Buttons(props){
 
     return(
         <div className="Buttons">
-        <button className="Toggler" /*onClick={}*/ >
-            I toggle
-        </button>
-        <button className="Incrementer" /* onClick={}*/ >
-            I increment
-        </button>
+            <button className="Toggler" onClick={props.toggler} >
+                I toggle
+            </button>
+            <button className="Incrementer" onClick={props.counter} >
+                I increment
+            </button>
         </div>
     )
 }
