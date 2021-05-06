c663b0fe704ff407e72fdda52b754fb5bfafacfc
carzetdesktop <carl.a.zettergren@gmail.com>
2021-05-06 13:04:09 +0200

    Button.js

---
+++ testproject/src/Buttons.js
@@ -0,0 +1,14 @@
+function Buttons(){
+
+    return(
+        <div className="Buttons">
+        <button className="Toggler">
+        I toggle
+        </button>
+        <button className="Incrementer">
+        I increment
+        </button>
+        </div>
+    )
+}
+export default Buttons;
