6298efd8330e8c4c00e2f1bf9f23dda720f6941d
carzetdesktop <carl.a.zettergren@gmail.com>
2021-05-06 13:42:37 +0200

    Better MVP, keeps structure of page nice and neat

--- testproject/src/Display.js
+++ testproject/src/Display.js
@@ -2,8 +2,10 @@ function Display(props){
 return(
     <div className="Display">
         
-        {props.displayImg ? <img src={props.imgSrc} alt=""/> : <></> }
+        {/*sätter css visibility till hidden så den bibehåler sidans proportioner*/}
+        <img className={props.displayImg ?  "": "invisible"  } src={props.imgSrc} alt=""/> 
         <h2>{props.number}</h2>
+        
     </div>
 )
 }
