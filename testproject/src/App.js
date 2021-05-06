90bb716bceced3e140b7b98483d4093e94223d9a
carzetdesktop <carl.a.zettergren@gmail.com>
2021-05-06 14:23:03 +0200

    express-generator based api with new added basicEndpoint

--- testproject/src/App.js
+++ testproject/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Buttons from './Buttons.js';
-import React, {useState} from 'react'; //nödvändig för att kunna uppdatera state
+import React, {useEffect, useState} from 'react'; //nödvändig för att kunna uppdatera state
 import Display from './Display';
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
   const saabBil = "https://okfl-static-files.s3.eu-north-1.amazonaws.com/klassiker/public/styles/main_image_full/public/images/2015/06/saab900turbo-1.jpg?itok=BAI6u-Fe"
   const [toggle, setToggle] = useState(true); //en boolean som blir true eller false
   const [count, setCounter] = useState(0); //en number som ökar
+  const [endpointResponse, setEndpointResponse] = useState("");
 
   //Funktioner vi slänger ned som props i barnen härifrån för att ge knapparna deras funktion
   const toggleFunc =()=>{
@@ -17,9 +18,20 @@ function App() {
   const counterFunc =()=>{
     setCounter(count + 1);
   }
+  const accessEndpoint = async() =>{
+    await fetch("http://localhost:9000/basicEndpoint")
+      .then(result => result.text())
+      .then(result => setEndpointResponse(result))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    accessEndpoint()
+  }, [])
 
   return (
     <div className="App">
+      <h1>{endpointResponse}</h1>
       <Buttons toggler={toggleFunc} counter={counterFunc}/> 
       <Display displayImg={toggle} imgSrc={saabBil} number={count}/>
     </div>
